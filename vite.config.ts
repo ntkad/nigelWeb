31894bcd1ffc5f536bf930fbbf7729363a42a0e7
ntkad <nigel.kadonzvo@ashesi.edu.gh>
2025-09-08 13:16:40 -0400

    hoyoo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nigelWeb/',  // <-- Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
